88974f09884156e18c6fbee6c9c52526abbe6efb
BYTE <ByteGamerYT@gmail.com>
2022-08-07 13:35:17 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,8 @@
+#Copyright (c) 2022, BYTE
+#All rights reserved.
+#
+#This source code is licensed under the BSD-style license found in the
+#LICENSE file in the root directory of this source tree. 
 
 from time import sleep
 from pyfiglet import Figlet
